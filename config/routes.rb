a9ffd9d065c70823673d67af83e0f91de1cc99d7
Katherine <katherinejc103@gmail.com>
2023-01-22 15:31:19 -0500

    commit tree 51cc91babf85cbabd4003aa2c693abf17bab6cc8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+
+  get '/users/:user_id/items', to: 'dog_houses#reviews_index'
+  get '/users/bad_id/items', to: 'dog_houses#review'
+  get '/users/:user_id/items/:id', to: 'dog_houses#review'
+  get '/users/:user_id/items', to: 'dog_houses#review'
+  post '/users/:user_id/items', to: 'dog_houses#review'
 end
